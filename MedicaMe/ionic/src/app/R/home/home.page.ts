22fcb2ff50af972c8e66ed9cc3525fabf74dd87a
anaclarapacheco <ana.ramos0406@gmail.com>
2021-09-09 23:18:40 -0300

    Home
    A tela: "Home (Responsável)" está 99% completa falta arrumar um erro no evento click: "close()" do menu lateral
    
    Co-Authored-By: Gabriel Santos de Sousa <86093696+Gabls@users.noreply.github.com>

--- MedicaMe/ionic/src/app/R/home/home.page.ts
+++ MedicaMe/ionic/src/app/R/home/home.page.ts
@@ -1,3 +1,4 @@
+import { FormatWidth, getLocaleDateFormat } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
@@ -5,20 +6,30 @@ import { NavController } from '@ionic/angular';
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
+
+
 })
 export class HomePage implements OnInit {
 
-  constructor(public navCtrl: NavController) { }
+  data: any = "";
 
-  //#region Abrir o Menu
+  constructor(public navCtrl: NavController) {
+    let nowData = new Date();
+
+    this.data = String(nowData.getDate()).padStart(2, '0') + '/' + String(nowData.getMonth() + 1).padStart(2, '0')  + '/' + nowData.getFullYear();
+   }
+
+  //#region Abrir e Fechar o Menu
   open()
   {
-    
+    var menu = document.getElementById('menu');
+    menu.classList.remove('invisivel');
   }
 
   close()
   {
-    
+    var menu = document.getElementById('menu');
+    menu.classList.add('invisivel');
   }
   //#endregion
 
