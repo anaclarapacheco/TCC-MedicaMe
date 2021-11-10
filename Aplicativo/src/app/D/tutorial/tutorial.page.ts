29c9655ad910e3ca240a8e6391ea1ba632c7179e
anaclarapacheco <ana.ramos0406@gmail.com>
2021-11-09 23:38:37 -0300

    Tutorial R e D - Slides
    Co-Authored-By: Gabriel Santos de Sousa <86093696+Gabls@users.noreply.github.com>

--- Aplicativo/src/app/D/tutorial/tutorial.page.ts
+++ Aplicativo/src/app/D/tutorial/tutorial.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavegationService } from 'src/app/services/navegation.service';
 import { ServidorService } from 'src/app/services/servidor.service';
+import { IonSlides } from '@ionic/angular';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial',
@@ -16,10 +18,37 @@ export class TutorialPage implements OnInit {
   }
   //#endregion
 
+  @ViewChild(IonSlides) slides: IonSlides;
+  public sliderOptions = {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true
+    }
+  };
+  
   //#region Próximo item do carrossel
   proximo()
   {
-    //
+    this.slideChanged();
+    const proximo = this.slides.slideNext();
+  }
+  //#endregion
+
+  //#region Verifica se é o último slide
+  public fim = false;
+  slideChanged(){
+    if (this.fim){
+      this.pular();
+    }
+
+    var verificarFim = this.slides.isEnd();
+    verificarFim.then(data => {
+      console.log(data);
+      if(data){
+        this.fim = true;
+      }
+    });
   }
   //#endregion
 
--- Aplicativo/src/app/R/tutorial/tutorial.page.ts
+++ Aplicativo/src/app/R/tutorial/tutorial.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NavegationService } from 'src/app/services/navegation.service';
 import { ServidorService } from 'src/app/services/servidor.service';
+import { IonSlides } from '@ionic/angular';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tutorial',
@@ -11,10 +13,37 @@ export class TutorialPage implements OnInit {
 
   constructor(private nav: NavegationService, private servidor: ServidorService){}
 
+  @ViewChild(IonSlides) slides: IonSlides;
+  public sliderOptions = {
+    pagination: {
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true
+    }
+  };
+
   //#region Próximo item do carrossel
   proximo()
   {
-    //
+    const proximo = this.slides.slideNext();
+    // this.slideChanged();
+  }
+  //#endregion
+
+  //#region Verifica se é o último slide
+  fim: boolean;
+  slideChanged(){
+    if (this.fim){
+      this.pular();
+    }
+
+    var verificarFim = this.slides.isEnd();
+    verificarFim.then(data => {
+      console.log(data);
+      if(data){
+        this.fim = true;
+      }
+    });
   }
   //#endregion
 
